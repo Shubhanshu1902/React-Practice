02f78a5454d23fa2f18741380e706e2cf14a4eb5
Shubhanshu1902 <shubhanshu1902@gmail.com>
2022-02-17 18:40:42 +0530

    arbnb complete

--- Arbnb_clone/src/App.js
+++ Arbnb_clone/src/App.js
@@ -3,8 +3,24 @@ import { Nav } from "./components/nav";
 import "./app.css";
 import Photogrid from "./components/photogrid";
 import Card from "./components/card";
+import data from "./Data/data";
 
 function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item}
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                country={item.location}
+                title={item.title}
+                price={item.price}
+                openSlots = {item.openSpots} 
+            />
+        );
+    });
+
     return (
         <div className="mainbody    ">
             <div
@@ -15,8 +31,8 @@ function App() {
                 }}
             >
                 <Nav />
-                <Photogrid />  
-                <Card /> 
+                <Photogrid />
+                <div className="cards">{cards}</div>
             </div>
         </div>
     );
--- Arbnb_clone/src/components/card.js
+++ Arbnb_clone/src/components/card.js
@@ -1,28 +1,36 @@
 import React from "react";
-import katie from "../images/katie.png";
 import { faStar } from "@fortawesome/fontawesome-free-solid";
 import "./styles/card.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Card = () => {
+const Card = props => {
+    let badgeText;
+    if (props.openSlots === 0) {
+        badgeText = "SOLD OUT";
+    } else if (props.country === "Online") {
+        badgeText = "ONLINE";
+    }
+
+    // console.log(img)
     return (
         <div className="card">
-            <img src={katie} alt="Katie" />
+            {badgeText && <div className="card-badge">{badgeText}</div>}
+            <img src={require(`../images/${props.img}`)} alt="Katie" />
             <div className="review">
                 <FontAwesomeIcon
                     icon={faStar}
                     color="#FE395C"
                     style={{ width: "14px", height: "14px" }}
                 />
-                <small className="stars">5.0</small>
-                <small className="number">(6)</small>
+                <small className="stars">{props.rating}</small>
+                <small className="number">({props.reviewCount})</small>
                 <div className="circle" />
-                <small className="country">USA</small>
+                <small className="country">{props.country}</small>
             </div>
 
-            <small className="title">Life lessons with Katie Zaferes</small>
+            <small className="title">{props.title}</small>
             <div className="price">
-                <b className="from_price">From $136</b>
+                <b className="from_price">From ${props.price}</b>
                 <small className="person">/ person</small>
             </div>
         </div>
